189dca852ff7cd21cf03971e47da44131316bf6d
Josh Naso <joshnaso@Joshs-MacBook-Pro.local>
2020-02-22 11:37:55 -0500

    ReactRPC wrapper function successfully compiles

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
 import { Component } from "react";
 //import wrapper from "/Users/joshnaso/Desktop/ReactRPC/ReactRPC/wrapper.js";
-const {reactRPC, wrapper} = require("testreactrpc");
+const reactRPC  = require("testreactrpc");
 const requests = require("../../helloworld_pb.js");
 
 const clients = require("../../helloworld_grpc_web_pb.js");
 
 reactRPC.build(requests, clients, "http://" + window.location.hostname + ":8080");
-//Working
-function wrapperr(WrappedComponent, reactR){
-    return class extends Component{
-        constructor(props){
-            super(props);
-        }
-
-        render(){
-        let obj = {};
-          for(let props in reactR.functions){
-            obj[props] = reactR.functions[props];
-          }
-            return <WrappedComponent {...obj} {...this.props}></WrappedComponent>
-
-        }
-    }
-}
 
 class App extends Component{
     constructor(props){
@@ -43,4 +26,4 @@ class App extends Component{
     }
 }
 
-export default wrapper(App, reactRPC);
+export default reactRPC.wrapper(App);
